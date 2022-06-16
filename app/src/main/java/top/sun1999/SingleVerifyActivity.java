802cfafc67373f0217898ecfd8e1cbbeb19da386
Zhaobaba918 <715702687@qq.com>
2022-06-16 23:20:23 +0800

    add 单人识别

--- app/src/main/java/top/sun1999/SingleVerifyActivity.java
+++ app/src/main/java/top/sun1999/SingleVerifyActivity.java
@@ -1,7 +1,6 @@
 package top.sun1999;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraX;
@@ -12,17 +11,16 @@ import androidx.camera.core.Preview;
 import androidx.camera.core.PreviewConfig;
 import androidx.camera.core.UseCase;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.ImageFormat;
 import android.graphics.Matrix;
 import android.graphics.Paint;
@@ -33,20 +31,20 @@ import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.provider.MediaStore;
-
-import android.util.Log;
 import android.util.Size;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
+import org.pytorch.IValue;
+import org.pytorch.MemoryFormat;
+import org.pytorch.Module;
+import org.pytorch.Tensor;
+import org.pytorch.torchvision.TensorImageUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -64,19 +62,8 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.concurrent.atomic.AtomicBoolean;
 
-import static java.lang.Math.PI;
-import static java.lang.Math.max;
-import static java.lang.Math.pow;
-
-import org.pytorch.IValue;
-import org.pytorch.MemoryFormat;
-import org.pytorch.Module;
-import org.pytorch.Tensor;
-import org.pytorch.torchvision.TensorImageUtils;
-
-public class VerifyActivity extends AppCompatActivity {
+public class SingleVerifyActivity extends AppCompatActivity {
     private static final String[] PERMISSIONS = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.CAMERA,
@@ -89,13 +76,13 @@ public class VerifyActivity extends AppCompatActivity {
     private final double threshold = 0.35;
     private final double nms_threshold = 0.7;
 
-
     private long startTime = 0;
     private long endTime = 0;
     private int width;
     private int height;
     private static final Paint boxPaint = new Paint();
 
+    //    private float[] myVec;
     private Module module;
 
     @Override
@@ -110,25 +97,50 @@ public class VerifyActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_single_verify);
+
         YOLOv4.init(getAssets());
         resultImageView = findViewById(R.id.imageView);
         thresholdTextview = findViewById(R.id.valTxtView);
         tvInfo = findViewById(R.id.tv_info);
 
-        thresholdTextview.setText("在掌纹库中进行匹配相似的掌纹");
+        thresholdTextview.setText("和选择的掌纹进行匹配");
 
         Button inference = findViewById(R.id.button);
         inference.setOnClickListener(view -> {
             finish();
         });
-
         startCamera();
         try {
             module = Module.load(assetFilePath(this, "best.pt"));
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        makeDialog("");
+
+        inputName();
+    }
+
+
+    int palmIndex;
+
+    public void inputName() {
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("选择想要识别的掌纹")//设置对话框的标题
+                .setSingleChoiceItems((ListAdapter) Util.names, 1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        palmIndex = which;
+                    }
+                })
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).create();
+        dialog.show();
     }
 
 
@@ -186,10 +198,6 @@ public class VerifyActivity extends AppCompatActivity {
     private class DetectAnalyzer implements ImageAnalysis.Analyzer {
         @Override
         public void analyze(ImageProxy image, final int rotationDegrees) {
-//            if (detecting.get() || detectPhoto.get()) {
-//                return;
-//            }
-//            detecting.set(true);
             final Bitmap bitmapsrc = imageToBitmap(image);  // 格式转换
             Thread detectThread = new Thread(() -> {
                 Matrix matrix = new Matrix();
@@ -245,27 +253,16 @@ public class VerifyActivity extends AppCompatActivity {
                         }
                         sum = (float) Math.sqrt(sum);
 
-                        List<Double> dot = new ArrayList<>();
-                        double tmpDot = 0f;
-                        for (double[] vect : Util.vecs) {
-                            for (int i = 0; i < vect.length; i++) {
-                                tmpDot += vec[i] / sum * vect[i];
-                            }
-                            dot.add(tmpDot);
+                        double dot = 0;
+                        for (int i = 0; i < vec.length; i++) {
+                            dot += vec[i] / sum * Util.vecs.get(palmIndex)[i];
                         }
-                        double maxDot = -1;
-                        for (int i = 0; i < dot.size(); i++) {
-                            if (dot.get(i) > maxDot) {
-                                maxDot = dot.get(i);
-                            }
-                        }
-                        String name = Util.names.get(dot.indexOf(maxDot));
 
                         endTime = System.currentTimeMillis();
-//                        String res1 = dot > 0.432 ? "掌纹匹配成功" : "掌纹匹配失败";
+                        String res1 = dot > 0.432 ? "掌纹匹配成功" : "掌纹匹配失败";
                         res = String.format(Locale.CHINESE,
-                                "%s，你被我认出来了！\n相似度: %.3f\nImgSize: %dx%d\nUseTime: %d ms",
-                                name, maxDot, mutableBitmap.getHeight(), mutableBitmap.getWidth(), endTime - startTime);
+                                "相似度: %.3f %s\nImgSize: %dx%d\nUseTime: %d ms",
+                                 dot, res1, mutableBitmap.getHeight(), mutableBitmap.getWidth(), endTime - startTime);
 
                     }
 
@@ -322,88 +319,6 @@ public class VerifyActivity extends AppCompatActivity {
             }
         }
     }
-
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-////        if (data == null) {
-////            return;
-////        }
-//
-//        detectPhoto.set(true);
-//
-////        byte[] photoByUrl = getPhotoByUrl(currentPhotoPath);
-////        Bitmap image = byteToBitmap(getPhotoByUrl(currentPhotoPath));
-////        Bitmap image = (Bitmap) photoByUrl;
-//        Bitmap image = getPicture(data.getData());
-//        //根据Uri显示本地图片
-////        photoImageView.setImageURI(uri);
-////        Log.d("success","路径：" + uri.toString());
-//
-//
-////        Bundle bundle = data.getExtras(); // 从data中取出传递回来缩略图的信息，图片质量差，适合传递小图片
-////        Bitmap image = (Bitmap) bundle.get("data"); // 将data中的信息流解析为Bitmap类型
-//
-////        Bitmap image = getPicture(data.getData());
-//
-//        startTime = System.currentTimeMillis();
-//        Box[] result = YOLOv4.detect(image, threshold, nms_threshold);
-//        Bitmap tmpimage = Util.extractROI(image, result);
-//        resultImageView.setImageBitmap(tmpimage);
-//        if (tmpimage == null) {
-//            tvInfo.setText("Yolo 识别失败");
-////            detectPhoto.set(false);
-//            return;
-//        }
-//
-//
-//        // preparing input tensor
-//        final Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(tmpimage,
-//                TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB, MemoryFormat.CHANNELS_LAST);
-//
-//        // running the model
-//        final Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
-//
-//
-//        if (true) {
-////            if (myVec == null) {
-//            // getting tensor content as java array of floats
-//            myVec = outputTensor.getDataAsFloatArray();
-//
-//            float sum = 0;
-//            for (float tmp : myVec) {
-//                sum += tmp * tmp;
-//            }
-//            sum = (float) Math.sqrt(sum);
-//            for (int i = 0; i < myVec.length; i++) {
-//                myVec[i] /= sum;
-//            }
-//            endTime = System.currentTimeMillis();
-//            Log.e("myVec", myVec.toString());
-//            tvInfo.setText(String.format(Locale.CHINESE,
-//                    "ImgSize: %dx%d\nUseTime: %d ms\n特征向量已保存",
-//                    image.getHeight(), image.getWidth(), endTime - startTime));
-//        } else {
-//            float[] vec = outputTensor.getDataAsFloatArray();
-//            float sum = 0;
-//            for (float tmp : vec) {
-//                sum += tmp * tmp;
-//            }
-//            sum = (float) Math.sqrt(sum);
-//
-//            float dot = 0;
-//            for (int i = 0; i < myVec.length; i++) {
-//                dot += vec[i] / sum * myVec[i];
-//            }
-//            endTime = System.currentTimeMillis();
-//            String res = dot > 0.432 ? "掌纹匹配成功" : "掌纹匹配失败";
-//            tvInfo.setText(String.format(Locale.CHINESE,
-//                    "ImgSize: %dx%d\nUseTime: %d ms\n相似度: %.3f %s ",
-//                    image.getHeight(), image.getWidth(), endTime - startTime, dot, res));
-//
-//        }
-////        detectPhoto.set(false);
-//    }
 
     public void makeDialog(String text) {
         //创建一个警告对话框
@@ -537,3 +452,6 @@ public class VerifyActivity extends AppCompatActivity {
         return bitmap;
     }
 }
+
+
+
